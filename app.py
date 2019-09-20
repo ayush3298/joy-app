928bb14d7276c968e590146984c3ec3d4126b422
Ayush <abairagi311@gmail.com>
2019-09-20 14:32:40 +0530

    added req

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect,url_for
 from database import database
+
 
 app = Flask(__name__)
 db = database()
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
 
@@ -17,8 +18,9 @@ def get_data():
         color = request.form["color"]
         cat_or_dog = request.form["dog_or_cat"]
         db.insert_data(name, color, cat_or_dog)
-    return render_template('index.html')
+    return redirect(url_for('index'))
+    # return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
